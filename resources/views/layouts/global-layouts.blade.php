933b450882decbf910bb3da7f0fa2229d70a3581
CherParent <dieumenshamamba@gmail.com>
2024-02-08 12:44:52 +0200

    reorganisation template

--- resources/views/layouts/global-layouts.blade.php
+++ resources/views/layouts/global-layouts.blade.php
@@ -1,25 +1,30 @@
-<html
-  lang="en"
-  class="light-style layout-menu-fixed layout-compact"
-  dir="ltr"
-  data-theme="theme-default""
-  data-template="vertical-menu-template-free">
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <meta charset="utf-8" />
-    <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
-    <title>CHABWAKI ONLINE</title>
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-    <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/bootstrap.min.css') !!}">
-    <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/slick.css') !!}">
-    <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/slick-theme.css') !!}"/>
-    <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/nouislider.min.css') !!}"/>
-    <link rel="stylesheet" href="{!! asset('vitrine/css/font-awesome.min.css') !!}">
-    <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/style.css') !!}"/>
-  </head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+  <title>Electro - HTML Ecommerce Template</title>
+
+  <!-- Google font -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/bootstrap.min.css') !!}">
+  <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/slick.css') !!}">
+  <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/slick-theme.css') !!}"/>
+  <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/nouislider.min.css') !!}"/>
+  <link rel="stylesheet" href="{!! asset('vitrine/css/font-awesome.min.css') !!}">
+  <link type="text/css" rel="stylesheet" href="{!! asset('vitrine/css/style.css') !!}"/>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
   @livewireStyles
-</head>
+
+  </head>
   <body>
     <div class="container-scroller">
         @include('layouts.partials._header')
@@ -27,12 +32,22 @@
         @include('layouts.partials._footer')
     </div>
     @livewireScripts
-  <script src="{!! asset('vitrine/js/jquery.min.js') !!}"></script>
-  <script src="{!! asset('vitrine/js/bootstrap.min.js') !!}"></script>
-  <script src="{!! asset('vitrine/js/slick.min.js') !!}"></script>
-  <script src="{!! asset('vitrine/js/nouislider.min.js') !!}"></script>
-  <script src="{!! asset('vitrine/js/jquery.zoom.min.js') !!}"></script>
-  <script src="{!! asset('vitrine/js/main.js') !!}"></script>
-  </body>
+  <!-- /FOOTER -->
+
+		<!-- jQuery Plugins -->
+		{{-- <script src="js/jquery.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/slick.min.js"></script>
+		<script src="js/nouislider.min.js"></script>
+		<script src="js/jquery.zoom.min.js"></script>
+		<script src="js/main.js"></script> --}}
+		<script src="{!! asset('vitrine/js/jquery.min.js') !!}"></script>
+    <script src="{!! asset('vitrine/js/bootstrap.min.js') !!}"></script>
+    <script src="{!! asset('vitrine/js/slick.min.js') !!}"></script>
+    <script src="{!! asset('vitrine/js/nouislider.min.js') !!}"></script>
+    <script src="{!! asset('vitrine/js/jquery.zoom.min.js') !!}"></script>
+    <script src="{!! asset('vitrine/js/main.js') !!}"></script>
+  
+	</body>
   @show
   </html>
--- resources/views/layouts/partials/_header.blade.php
+++ resources/views/layouts/partials/_header.blade.php
@@ -1,109 +1,144 @@
-<div id="top-header">
-    <div class="container">
-        <ul class="header-links pull-left">
-            <li><a href="#"><i class="fa fa-phone"></i> +021-95-51-84</a></li>
-            <li><a href="#"><i class="fa fa-envelope-o"></i> email@email.com</a></li>
-            <li><a href="#"><i class="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
-        </ul>
-        <ul class="header-links pull-right">
-            <li><a href="#"><i class="fa fa-dollar"></i> USD</a></li>
-            <li><a href="#"><i class="fa fa-user-o"></i> Mon Compte</a></li>
-            {{-- <li><a href="{{route('client.creer_boutique')}}"><i class="fa fa-user-o"></i> Créer Boutique</a></li> --}}
-        </ul>
+<header>
+    <!-- TOP HEADER -->
+    <div id="top-header">
+        <div class="container">
+            <ul class="header-links pull-left">
+                <li><a href="#"><i class="fa fa-phone"></i> +021-95-51-84</a></li>
+                <li><a href="#"><i class="fa fa-envelope-o"></i> email@email.com</a></li>
+                <li><a href="#"><i class="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
+            </ul>
+            <ul class="header-links pull-right">
+                <li><a href="#"><i class="fa fa-dollar"></i> USD</a></li>
+                <li><a href="#"><i class="fa fa-user-o"></i> My Account</a></li>
+            </ul>
+        </div>
     </div>
-</div>
-<div id="header">
-            <!-- container -->
-            <div class="container">
-                <!-- row -->
-                <div class="row">
-                    <!-- LOGO -->
-                    <div class="col-md-3">
-                        <div class="header-logo">
-                            <a href="#" class="logo">
-                                <!--img src="{{ asset('vitrine/img/logo.png') }}" alt=""-->
-                                <span style="font-size: 40px; font-weight: bold;color: white;">Chabwaki</span>
+    <!-- /TOP HEADER -->
+
+    <!-- MAIN HEADER -->
+    <div id="header">
+        <!-- container -->
+        <div class="container">
+            <!-- row -->
+            <div class="row">
+                <!-- LOGO -->
+                <div class="col-md-3">
+                    <div class="header-logo">
+                        <a href="#" class="logo">
+                            <img src="./img/logo.png" alt="">
+                        </a>
+                    </div>
+                </div>
+                <!-- /LOGO -->
+
+                <!-- SEARCH BAR -->
+                <div class="col-md-6">
+                    <div class="header-search">
+                        <form>
+                            <select class="input-select">
+                                <option value="0">All Categories</option>
+                                <option value="1">Category 01</option>
+                                <option value="1">Category 02</option>
+                            </select>
+                            <input class="input" placeholder="Search here">
+                            <button class="search-btn">Search</button>
+                        </form>
+                    </div>
+                </div>
+                <!-- /SEARCH BAR -->
+
+                <!-- ACCOUNT -->
+                <div class="col-md-3 clearfix">
+                    <div class="header-ctn">
+                        <!-- Wishlist -->
+                        <div>
+                            <a href="#">
+                                <i class="fa fa-heart-o"></i>
+                                <span>Your Wishlist</span>
+                                <div class="qty">2</div>
                             </a>
                         </div>
-                    </div>
-                    <!-- /LOGO -->
+                        <!-- /Wishlist -->
 
-                    <!-- SEARCH BAR -->
-                    <div class="col-md-6">
-                        <div class="header-search">
-                            <form>
-                                <select class="input-select">
-                                    <option value="0">Catégories</option>
-                                    <option value="1">Catégorie 01</option>
-                                    <option value="1">Catégorie 02</option>
-                                </select>
-                                <input class="input" placeholder="Rechercher ici">
-                                <button class="search-btn">Rechercher</button>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- /SEARCH BAR -->
+                        <!-- Cart -->
+                        <div class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
+                                <i class="fa fa-shopping-cart"></i>
+                                <span>Your Cart</span>
+                                <div class="qty">3</div>
+                            </a>
+                            <div class="cart-dropdown">
+                                <div class="cart-list">
+                                    <div class="product-widget">
+                                        <div class="product-img">
+                                            <img src="./img/product01.png" alt="">
+                                        </div>
+                                        <div class="product-body">
+                                            <h3 class="product-name"><a href="#">product name goes here</a></h3>
+                                            <h4 class="product-price"><span class="qty">1x</span>$980.00</h4>
+                                        </div>
+                                        <button class="delete"><i class="fa fa-close"></i></button>
+                                    </div>
 
-                    <!-- ACCOUNT -->
-                    <div class="col-md-3 clearfix">
-                        <div class="header-ctn">
-                            <!-- Wishlist -->
-                            <div>
-                                <a href="#">
-                                    <i class="fa fa-heart-o"></i>
-                                    <span>Votre list</span>
-                                    <div class="qty">2</div>
-                                </a>
-                            </div>
-                            <!-- /Wishlist -->
-
-                            <!-- Cart -->
-                            <div class="dropdown">
-                                <a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
-                                    <i class="fa fa-shopping-cart"></i>
-                                    <span>Votre Panier</span>
-                                    <div class="qty">3</div>
-                                </a>
-                                <div class="cart-dropdown">
-                                    <!-- ... (le reste du code) -->
+                                    <div class="product-widget">
+                                        <div class="product-img">
+                                            <img src="./img/product02.png" alt="">
+                                        </div>
+                                        <div class="product-body">
+                                            <h3 class="product-name"><a href="#">product name goes here</a></h3>
+                                            <h4 class="product-price"><span class="qty">3x</span>$980.00</h4>
+                                        </div>
+                                        <button class="delete"><i class="fa fa-close"></i></button>
+                                    </div>
+                                </div>
+                                <div class="cart-summary">
+                                    <small>3 Item(s) selected</small>
+                                    <h5>SUBTOTAL: $2940.00</h5>
+                                </div>
+                                <div class="cart-btns">
+                                    <a href="#">View Cart</a>
+                                    <a href="#">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
-                            <!-- /Cart -->
-
-                            <!-- Menu Toogle -->
-                            <div class="menu-toggle">
-                                <a href="#">
-                                    <i class="fa fa-bars"></i>
-                                    <span>Menu</span>
-                                </a>
-                            </div>
-                            <!-- /Menu Toogle -->
                         </div>
+                        <!-- /Cart -->
+
+                        <!-- Menu Toogle -->
+                        <div class="menu-toggle">
+                            <a href="#">
+                                <i class="fa fa-bars"></i>
+                                <span>Menu</span>
+                            </a>
+                        </div>
+                        <!-- /Menu Toogle -->
                     </div>
-                    <!-- /ACCOUNT -->
                 </div>
-                <!-- row -->
+                <!-- /ACCOUNT -->
             </div>
-            <!-- container -->
+            <!-- row -->
         </div>
-        <nav id="navigation">
-            <!-- container -->
-            <div class="container">
-                <!-- responsive-nav -->
-                <div id="responsive-nav">
-                    <!-- NAV -->
-                    <ul class="main-nav nav navbar-nav">
-                        {{-- <li class="active"><a href="{{route('client.index')}}">Accueil</a></li>
-                        <li><a href="{{route('client.boutique')}}">Boutique</a></li> --}}
-                        <li><a href="#">Catégories</a></li>
-                        <li><a href="#">Assistance</a></li>
-                        <li><a href="#">Mon Compte</a></li>
-                        <li><a href="#">Blog</a></li>
-                        <li><a href="#">À Propos</a></li>
-                    </ul>
-                    <!-- /NAV -->
-                </div>
-                <!-- /responsive-nav -->
-            </div>
-            <!-- /container -->
-        </nav>
+        <!-- container -->
+    </div>
+    <!-- /MAIN HEADER -->
+</header>
+<nav id="navigation">
+    <!-- container -->
+    <div class="container">
+        <!-- responsive-nav -->
+        <div id="responsive-nav">
+            <!-- NAV -->
+            <ul class="main-nav nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Hot Deals</a></li>
+                <li><a href="#">Categories</a></li>
+                <li><a href="#">Laptops</a></li>
+                <li><a href="#">Smartphones</a></li>
+                <li><a href="#">Cameras</a></li>
+                <li><a href="#">Accessories</a></li>
+            </ul>
+            <!-- /NAV -->
+        </div>
+        <!-- /responsive-nav -->
+    </div>
+    <!-- /container -->
+</nav>
